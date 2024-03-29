7abfcbb88a3840977ba7a8ef3b0b4bf0f398e6c8
Nicolai Kürtös <69422846+NicolaiKuertoes@users.noreply.github.com>
2024-03-29 14:47:19 +0100

    fix colors

--- Satznummern.js
+++ Satznummern.js
@@ -13,7 +13,7 @@ if (URL.match(pattern)) {
             abs_nr.lastIndex = 0; // fix for weird regex test() bug https://stackoverflow.com/questions/3891641/regex-test-only-works-every-other-time
             if (abs_nr.test(satz)) {
                 let satz_part = satz.split(abs_nr).filter(elm => elm);
-                newAbsatz.push(satz_part[0] + " <sup style='color: red;'>" + (nr+1) + "</sup>" + satz_part[1].replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.').trim());
+                newAbsatz.push(satz_part[0] + " <sup style='color: #FF00BF;'>" + (nr+1) + "</sup>" + satz_part[1].replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.').trim());
             
             } else {
                 newAbsatz.push("<sup style='color: #FF00BF;'>" + (nr+1) + "</sup>" +satz.replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.'));
