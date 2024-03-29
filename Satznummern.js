2bfbe004938a4b7f0b735163b0a166483ceb15a8
Nicolai Kürtös <69422846+NicolaiKuertoes@users.noreply.github.com>
2024-03-29 14:30:16 +0100

    fix absatznummern

--- Satznummern.js
+++ Satznummern.js
@@ -1,11 +1,21 @@
-const pattern = /https:\/\/www.gesetze-im-internet.de\/\w+\/\w+.html/g;
+const pattern = /https:\/\/www\.gesetze-im-internet\.de\/\w+\/\w+\.html.*/g;
 let URL = window.location.href;
+
 if (URL.match(pattern)) {
-    const elem = document.getElementsByClassName('jurAbsatz')[0];
-    const text = elem.textContent.split('. ');
-    let beautyfied = [];
-    for (const [nr, satz] of text.entries()) {
-        beautyfied.push("<sup>"+(nr+1)+"</sup>"+satz+".");
-    }
-    elem.innerHTML = beautyfied.join(' ').replace('..', '.');
+    const absaetze = document.getElementsByClassName('jurAbsatz');
+    const abs_nr = /^(\(\w+\))/
+    Array.from(absaetze).forEach((absatz) => {
+        let saetze = absatz.innerHTML.split(/(?=\. [A-Z]{1})/g);
+        let newAbsatz = [];
+        for (const [nr, satz] of saetze.entries()) {
+            abs_nr.lastIndex = 0; // fix for weird regex test() bug https://stackoverflow.com/questions/3891641/regex-test-only-works-every-other-time
+            if (abs_nr.test(satz)) {
+                let satz_part = satz.split(abs_nr).filter(elm => elm);
+                newAbsatz.push(satz_part[0] + " <sup style='color: red;'>" + (nr+1) + "</sup>" + satz_part[1].replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.').trim());
+            } else {
+                newAbsatz.push("<sup style='color: red;'>" + (nr+1) + "</sup>" +satz.replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.'));
+            }
+        }
+        absatz.innerHTML = newAbsatz.join(' ');
+    });
 }
