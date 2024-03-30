5614bd5e857867ff29b39628ee3049e5e6436afa
Nicolai Kürtös <69422846+NicolaiKuertoes@users.noreply.github.com>
2024-03-30 10:46:33 +0100

    fix issues with absatznummern

--- Satznummern.js
+++ Satznummern.js
@@ -1,26 +1,54 @@
 const pattern = /https:\/\/www\.gesetze-im-internet\.de\/\w+\/\w+\.html.*/g;
 let URL = window.location.href;
 
-if (URL.match(pattern)) {
-    const absaetze = document.getElementsByClassName('jurAbsatz');
-    const abs_nr = /^(\(\w+\))/
 
-    Array.from(absaetze).forEach((absatz) => {
-        let saetze = absatz.innerHTML.split(/(?=\.\s+[A-Z]{1})/g);
-        if(saetze.length >= 2) {
-        let newAbsatz = [];
-        for (const [nr, satz] of saetze.entries()) {
-            abs_nr.lastIndex = 0; // fix for weird regex test() bug https://stackoverflow.com/questions/3891641/regex-test-only-works-every-other-time
-            if (abs_nr.test(satz)) {
-                let satz_part = satz.split(abs_nr).filter(elm => elm);
-                newAbsatz.push(satz_part[0] + " <sup style='color: #FF00BF;'>" + (nr+1) + "</sup>" + satz_part[1].replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.').trim());
-            } else {
-                newAbsatz.push("<sup style='color: #FF00BF;'>" + (nr+1) + "</sup>" +satz.replace(/^\.\s+/, '').replace(/$/, '.').replace(/\.+$/, '.'));
-            }
-        }
-        if (!absatz.innerHTML.match(/<\w+>/g)) {
-            absatz.innerHTML = newAbsatz.join(' ');
-        }
+function has_numbers(absatz) {
+    return absatz.innerHTML.includes("<dl");
+}
+
+
+function get_saetze(absatz) {
+    let saetze = absatz.textContent.split(/(?<=[^A-Z].[.?!]) +(?=[§A-Z])/g);
+    return saetze;
+}
+
+
+function format_sup(satz, nr) {
+    if (satz.startsWith('(')) {
+        let teil = satz.split(/^(\(.+\))/).filter(elem => elem);
+        return teil[0] + " <sup style='color: red;'>"+nr+"</sup>"+teil[1].trim();
+    } else {
+        return "<sup style='color: red;'>"+nr+"</sup>"+satz;
+    }
+}
+
+
+if (URL.match(pattern)) {
+    const absaetze = Array.from(document.getElementsByClassName('jurAbsatz'));
+
+    absaetze.forEach((absatz) => {
+        if (has_numbers(absatz)) {
+            let new_absatz = [];
+            let saetze = absatz.innerHTML.split(/(\<dl.*\<\/dl\>)/g);
+            let nr = 1;
+            saetze.forEach((satz) => {
+                if (satz.endsWith('</dl>') ||  satz.match(/^[a-z]/)) {
+                    new_absatz.push(satz);
+                } else {
+                    new_absatz.push(format_sup(satz, nr));
+                    ++nr;
+                }
+            });
+            absatz.innerHTML = new_absatz.join(' ');
+        } else if (get_saetze(absatz).length > 1) {
+            let new_absatz = [];
+            let saetze = get_saetze(absatz);
+            let nr = 1;
+            saetze.forEach((satz) => {
+                new_absatz.push(format_sup(satz, nr));
+                ++nr;
+            });
+            absatz.innerHTML = new_absatz.join(' ');
         }
     });
 }
